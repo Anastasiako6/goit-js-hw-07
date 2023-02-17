0ca288e9ee1f0a16ad2924221d542d5c52dc151d
Anastasiako6 <anastasiakorotenko3@gmail.com>
2023-02-18 00:20:15 +0200

    add script and link

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const imgContainer = galleryItems.reduce((acum, item) => {
+    return (acum += ` <div class = "gallery__item"> 
+    <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+    </a>
+</div>`);
+}, '');
+gallery.insertAdjacentHTML('beforeend', imgContainer);
+
+
